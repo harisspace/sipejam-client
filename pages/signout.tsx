fe97723ca3f2955ff783d887384496dc014026f3
haris akbar <harisakbar04@gmail.com>
2021-07-26 10:58:23 +0700

    signout done

--- pages/signout.tsx
+++ pages/signout.tsx
@@ -1,15 +1,18 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { signoutRequest } from "../api/user.request";
 
 const Signout = () => {
   const router = useRouter();
+
+  const signoutFetch = async () => {
+    await fetch(`${process.env.NEXT_PUBLIC_BASE_URL_API}/user/signout`, { credentials: "include", method: "GET" });
+  };
 
   useEffect(() => {
     if (!router.isReady) return;
 
     try {
-      signoutRequest();
+      signoutFetch();
     } catch (err) {
       console.log(err);
       router.back();
