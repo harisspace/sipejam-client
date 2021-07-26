06f037b9407478b7e491b88b3c36d4d4e181c0f9
haris akbar <harisakbar04@gmail.com>
2021-07-26 10:36:06 +0700

    now support google oauth for authentication

--- components/Templates/GoogleOAuth.tsx
+++ components/Templates/GoogleOAuth.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
+import Link from "next/link";
 
 const GoogleOAuth = () => {
   const router = useRouter();
@@ -8,14 +9,16 @@ const GoogleOAuth = () => {
 
   return (
     <div className="bg-blue-500  w-8/12 rounded-sm shadow-lg cursor-pointer">
-      <a href="/auth/oauth/google" className="p-1 flex items-center">
-        <div className="p-1 bg-white">
-          <Image src="/images/google.png" alt="google" className="bg-contain" width={30} height={30} />
-        </div>
-        <span className="text-white ml-4 font-bold text-md tracking-normal">
-          Sign {path === "signin" ? "In" : "Up"} with google
-        </span>
-      </a>
+      <Link href="/auth/oauth/google">
+        <a className="p-1 flex items-center">
+          <div className="p-1 bg-white">
+            <Image src="/images/google.png" alt="google" className="bg-contain" width={30} height={30} />
+          </div>
+          <span className="text-white ml-4 font-bold text-md tracking-normal">
+            Sign {path === "signin" ? "In" : "Up"} with google
+          </span>
+        </a>
+      </Link>
     </div>
   );
 };
